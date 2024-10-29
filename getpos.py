422de136134ce2ac5d4a1f195eecaec85a4ec725
zhou <zhou@gmail.com>
2024-10-29 20:28:13 +0800

    modified some parameter in tpprice,uplRatio < 0.4 and mgnRatio< 2.9

--- getpos.py
+++ getpos.py
@@ -6,9 +6,9 @@ import logging
 from regular_err import SpecialJumpException
 
 if __name__ == '__main__':
-    api_key = ""
-    secret_key = ""
-    passphrase = ""
+    api_key = "ba7f444f-e83e-4dd1-8507-bf8dd9033cbc"
+    secret_key = "D5474EF76B0A7397BFD26B9656006480"
+    passphrase = "TgTB+pJoM!d20F"
 
     # 设置日志配置
     logging.basicConfig(filename='output.log', level=logging.INFO,
@@ -56,7 +56,7 @@ if __name__ == '__main__':
             logging.info("------------------------")
 
             #如果收益率>=-30%,补一次，但要判断是否已经存在一样的委托
-            if float(pos['uplRatio'])<=-0.4 :
+            if float(pos['uplRatio'])<=-0.4 or pos['mgnRatio']< 2.9:
                 #获取该合约未完成订单
                 result1 = tradeAPI.get_order_list(instType='SWAP',instId=pos['symbol'])
                 orderlist = parse_orderlist(json.dumps(result1))
@@ -68,13 +68,11 @@ if __name__ == '__main__':
                     #计算止盈价格，如果保证金低于50，止盈25% 5倍杠杆就是挂单价格*1.05，如果保证金低于100 止盈20% 挂单价格*1.04 如果是低于200 止盈15% 即挂单价格*1.03 如果是低于300 止盈10% 即挂单价格*1.02 其余挂单价格*1.01
                     if pos['side'] == 'buy':
                         #tpprice = price*0.99 if pos['side'] == 'sell' else price*1.01
-                        tpprice = price*1.05 if float(pos['margin']) <= 50 \
-                            else price*1.04 if float(pos['margin']) <= 100 \
+                        tpprice = price*1.04 if float(pos['margin']) <= 100 \
                             else price*1.03 if float(pos['margin']) <= 200 \
                             else price*1.02 if float(pos['margin']) <= 300 else price*1.01
                     else:
-                        tpprice = price*0.95 if float(pos['margin']) <= 50 \
-                            else price*0.96 if float(pos['margin']) <= 100 \
+                        tpprice = price*0.96 if float(pos['margin']) <= 100 \
                             else price*0.97 if float(pos['margin']) <= 200 \
                             else price*0.98 if float(pos['margin']) <= 300 else price*0.99
                     logging.info (f"price: {price} tpprice: {tpprice}")
@@ -104,6 +102,6 @@ if __name__ == '__main__':
                 logging.info(json.dumps(close_pos_reslut))
             #再次尝试下单
             order_reslut = tradeAPI.place_order(instId=pos['symbol'], tdMode='isolated', side=pos['side'],
-                                   ordType='limit', sz=abs(pos['size']), px = price, tpTriggerPx=tpprice,tpOrdPx=-1)
+                                   ordType='limit', sz=abs(pos['size']/2), px = price, tpTriggerPx=tpprice,tpOrdPx=-1)
             logging.info(json.dumps(order_reslut))
     
