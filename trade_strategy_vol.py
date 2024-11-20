4de944296453a2d71f9cc5be8e2492b3d78854e3
zhou <zhou@gmail.com>
2024-11-20 23:00:37 +0800

    trade_strategy_vol

--- trade_strategy_vol.py
+++ trade_strategy_vol.py
@@ -45,15 +45,17 @@ class DeltaNeutralStrategy():
         self.base_position_size = 1.0  # 基础仓位大小
         self.profit_target = 0.015  # 止盈目标 1.5%
         self.stop_loss = 0.08  # 止损线 8%
-        self.max_position = 3  # 最大持仓倍数
-        self.min_position = 1  # 最小持仓倍数
-        self.contract_size_multiplier =None
+        self.max_value = 100 #最大持仓金额
+        self.step_value = 10 #每次开仓金额
+        #self.max_position = 100  # 最大持仓
+        #self.min_position = 1  # 最小持仓倍数
+        #self.contract_size_multiplier =None
         # 技术指标参数
-        self.rsi_period = 14
-        self.rsi_overbought = 70
-        self.rsi_oversold = 30
-        self.ma_fast = 20
-        self.ma_slow = 50
+        # self.rsi_period = 14
+        # self.rsi_overbought = 70
+        # self.rsi_oversold = 30
+        # self.ma_fast = 20
+        # self.ma_slow = 50
         
         # 仓位记录
         self.spot_position = 0
@@ -219,21 +221,21 @@ class DeltaNeutralStrategy():
             print(f"生成交易信号时出错: {str(e)}")
             return None
    
-    def check_entry_conditions(self, indicators):
-        """检查入场条件"""
-        # RSI超卖
-        rsi_buy = indicators['rsi'] < self.rsi_oversold
-        # 快线在慢线上方
-        ma_trend_up = indicators['ma_fast'] > indicators['ma_slow']
-        # MACD金叉
-        macd_cross = indicators['macd'] > indicators['signal']
-        # 价格接近布林带下轨
-        price_near_lower = indicators['current_price'] < indicators['bb_lower'] * 1.01
-        logging.info(f"RSI超卖,rsi：{indicators['rsi']},ris_oversold:{self.rsi_oversold}")
-        logging.info(f"快线在慢线上方,ma_fast：{indicators['ma_fast']},ma_slow:{indicators['ma_slow']}")
-        logging.info(f"MACD金叉：{indicators['macd']}，signal:{indicators['signal']}")
-        logging.info(f"价格接近布林带下轨,bb_lower：{indicators['bb_lower']},price:{indicators['current_price']}")
-        return rsi_buy and ma_trend_up and macd_cross and price_near_lower
+    # def check_entry_conditions(self, indicators):
+    #     """检查入场条件"""
+    #     # RSI超卖
+    #     rsi_buy = indicators['rsi'] < self.rsi_oversold
+    #     # 快线在慢线上方
+    #     ma_trend_up = indicators['ma_fast'] > indicators['ma_slow']
+    #     # MACD金叉
+    #     macd_cross = indicators['macd'] > indicators['signal']
+    #     # 价格接近布林带下轨
+    #     price_near_lower = indicators['current_price'] < indicators['bb_lower'] * 1.01
+    #     logging.info(f"RSI超卖,rsi：{indicators['rsi']},ris_oversold:{self.rsi_oversold}")
+    #     logging.info(f"快线在慢线上方,ma_fast：{indicators['ma_fast']},ma_slow:{indicators['ma_slow']}")
+    #     logging.info(f"MACD金叉：{indicators['macd']}，signal:{indicators['signal']}")
+    #     logging.info(f"价格接近布林带下轨,bb_lower：{indicators['bb_lower']},price:{indicators['current_price']}")
+    #     return rsi_buy and ma_trend_up and macd_cross and price_near_lower
     
     def check_exit_conditions(self, indicators, entry_price):
         """检查出场条件"""
@@ -254,38 +256,38 @@ class DeltaNeutralStrategy():
             
         return False, None
  
-    def get_contract_multiplier(self):
-        """获取合约最小下单单位"""
-        try:
-            # 尝试从API获取
-            markets = self.exchange.load_markets()
-            contract_market = markets[self.symbol]
+    # def get_contract_multiplier(self):
+    #     """获取合约最小下单单位"""
+    #     try:
+    #         # 尝试从API获取
+    #         markets = self.exchange.load_markets()
+    #         contract_market = markets[self.symbol]
             
-            # 尝试从市场信息中获取合约乘数
-            multiplier = None
-            if 'contractSize' in contract_market:
-                multiplier = contract_market['contractSize']
-            elif 'lot' in contract_market:
-                multiplier = contract_market['lot']
+    #         # 尝试从市场信息中获取合约乘数
+    #         multiplier = None
+    #         if 'contractSize' in contract_market:
+    #             multiplier = contract_market['contractSize']
+    #         elif 'lot' in contract_market:
+    #             multiplier = contract_market['lot']
             
-            if multiplier is not None:
-                logging.info(f"从API获取到{self.symbol}的合约乘数: {multiplier}")
-                return multiplier
+    #         if multiplier is not None:
+    #             logging.info(f"从API获取到{self.symbol}的合约乘数: {multiplier}")
+    #             return multiplier
                 
-            # 如果API没有提供，使用手动设置的值
-            if self.contract_size_multiplier is not None:
-                logging.info(f"使用手动设置的{self.symbol}合约乘数: {self.contract_size_multiplier}")
-                return self.contract_size_multiplier
-                # 如果需要手动设置合约乘数
-            strategy.contract_size_multiplier = 100  # 对于HBAR设置为100
-            # 如果都没有，使用默认值1
-            logging.info(f"未能获取{self.symbol}合约乘数，使用默认值: 1")
-            return 1
+    #         # 如果API没有提供，使用手动设置的值
+    #         if self.contract_size_multiplier is not None:
+    #             logging.info(f"使用手动设置的{self.symbol}合约乘数: {self.contract_size_multiplier}")
+    #             return self.contract_size_multiplier
+    #             # 如果需要手动设置合约乘数
+    #         strategy.contract_size_multiplier = 100  # 对于HBAR设置为100
+    #         # 如果都没有，使用默认值1
+    #         logging.info(f"未能获取{self.symbol}合约乘数，使用默认值: 1")
+    #         return 1
         
-        except Exception as e:
-            logging.info(f"获取合约乘数失败: {str(e)}")
-            # 如果出错，使用手动设置的值或默认值
-            return self.contract_size_multiplier or 1
+    #     except Exception as e:
+    #         logging.info(f"获取合约乘数失败: {str(e)}")
+    #         # 如果出错，使用手动设置的值或默认值
+    #         return self.contract_size_multiplier or 1
 
     def calculate_position_size(self):
         """计算合适的开仓数量"""
@@ -299,33 +301,36 @@ class DeltaNeutralStrategy():
             # 获取当前BTC价格
             ticker = self.exchange.fetch_ticker(self.symbol)
             current_price = ticker['last']
+
+            #获取当前现货持仓
+            positions = self.exchange.fetch_positions([self.inst_id_spot])
             
             #计算最大允许开仓金额（账户总值的10%）
-            max_position_value = spot_usdt * 0.1
+            
             logging.info(f"账户USDT余额: {spot_usdt}")
-            logging.info(f"最大允许开仓金额: {max_position_value} USDT")     
+            #logging.info(f"最大允许开仓金额: {max_position_value} USDT")     
 
             # 计算现货可以开的数量
-            spot_size = max_position_value / current_price
+            spot_size = self.step_value / current_price
             
             # 计算合约可以开的张数（向下取整到10的倍数）
             # 由于合约一张=10个单位，所以需要除以10
-            contract_size = (spot_size // 10) * 10
+            #contract_size = (spot_size // 10) * 10
             
             # 确保合约张数至少是10（欧易HBAR最小10张）
-            if contract_size < 10:
-                print("可用资金不足以开立最小合约仓位（10张）")
-                return 0
+            #if contract_size < 10:
+            #    print("可用资金不足以开立最小合约仓位（10张）")
+            #    return 0
                  
             # 重新计算实际的现货数量，使其与合约数量匹配
-            spot_size = contract_size  # 现货数量等于合约张数对应的数量
+            #spot_size = contract_size  # 现货数量等于合约张数对应的数量
           
             logging.info(f"当前{self.symbol}价格: {current_price} USDT")
             logging.info(f"现货下单数量: {spot_size}")
-            logging.info(f"合约下单张数: {contract_size/10} 张")  # 除以10显示实际张数
+            #logging.info(f"合约下单张数: {contract_size/10} 张")  # 除以10显示实际张数
             logging.info(f"预计使用保证金: {spot_size * current_price} USDT")
             
-            return float(spot_size)
+            return float(spot_size),spot_usdt
             
         except Exception as e:
             logging.info(f"计算仓位大小时出错: {str(e)}")
@@ -335,7 +340,7 @@ class DeltaNeutralStrategy():
         """初始化基础对冲仓位"""
         try:
             # 计算合适的开仓数量
-            position_size = self.calculate_position_size()
+            position_size, balance = self.calculate_position_size()
             
             if position_size <= 0:
                 logging.info("没有足够的资金开仓")
@@ -343,16 +348,16 @@ class DeltaNeutralStrategy():
                 
             
             #现货买入
-            # spot_order = self.exchange.create_order(
-            #     symbol=self.symbol,
-            #     type='market',
-            #     side='buy',
-            #     amount=position_size,  # 使用调整后的数量
-            #     params={
-            #         'instId': self.inst_id_spot,
-            #         'tdMode': 'cash'
-            #     }
-            # )
+            spot_order = self.exchange.create_order(
+                symbol=self.symbol,
+                type='market',
+                side='buy',
+                amount=position_size,  # 使用调整后的数量
+                params={
+                    'instId': self.inst_id_spot,
+                    'tdMode': 'cash'
+                }
+            )
             
             # 合约做空
             # 先设置杠杆
@@ -375,10 +380,10 @@ class DeltaNeutralStrategy():
             self.spot_position = float(position_size)
             self.futures_position = -float(position_size)
             
-            logging.info(f"基础对冲仓位建立完成:")
+            # logging.info(f"基础对冲仓位建立完成:")
             # print(f"现货订单: {spot_order}")
             # print(f"合约订单: {futures_order}")
-            logging.info(f"现货持仓: {self.spot_position}, 合约持仓: {self.futures_position}")
+            logging.info(f"现货持仓: {self.spot_position}")
             
             return True
             
@@ -398,47 +403,51 @@ class DeltaNeutralStrategy():
                     continue
                 
                 # 计算当前可用的交易数量
-                trade_size = self.spot_position*0.1
-                if trade_size <= 0:
+                # 获取当前持仓
+                positions,balance = self.calculate_position_size()
+                if positions <= 0:
                     logging.info("可用资金不足，暂停交易")
                     return
                 
                 # 检查是否可以增加新的交易仓位
-                if len(self.trading_positions) < (self.max_position - self.min_position):
-                    if self.check_entry_conditions(indicators):
-                        # 执行买入
-                        entry_price = indicators['current_price']
-                        
+                # 根据信号执行交易
+                if signals['signal'] == 'buy' and signals['strength'] >= 2:
+                    # 检查是否有足够的USDT
+                    ticker = self.exchange.fetch_ticker(self.symbol)
+                    required_usdt = positions * ticker['last']
+                    
+                    if balance >= required_usdt:
                         order = self.exchange.create_order(
                             symbol=self.symbol,
                             type='market',
                             side='buy',
-                            amount=trade_size,
+                            amount=positions,
                             params={
                                 'instId': self.inst_id_spot,
                                 'tdMode': 'cash'
                             }
                         )
+                        print(f"买入信号执行: 数量={positions}, 强度={signals['strength']}")
                         logging.info(f"现货订单: {order}")
                         self.trading_positions.append({
-                            'price': entry_price,
-                            'size': float(trade_size),
+                            'price': ticker['last'],
+                            'size': float(positions),
                             'timestamp': datetime.now()
                         })
-                        logging.info(f"新增交易仓位: 价格{entry_price}, 数量{trade_size}")
+                        logging.info(f"新增交易仓位: 价格{ticker['last']}, 数量{positions}")
                     
-                    # 检查现有仓位是否需要平仓
-                    for pos in self.trading_positions[:]:  # 使用切片创建副本进行遍历
-                        should_exit, reason = self.check_exit_conditions(indicators, pos['price'])
-                        if should_exit:
-                            # 执行卖出
-                            order = self.exchange.create_market_sell_order(
-                                self.symbol,
-                                pos['size']
-                            )
-                            profit = (indicators['current_price'] - pos['price']) * pos['size']
-                            self.trading_positions.remove(pos)
-                            logging.info(f"平仓: 原因{reason}, 盈亏{profit}")
+                    # # 检查现有仓位是否需要平仓
+                    # for pos in self.trading_positions[:]:  # 使用切片创建副本进行遍历
+                    #     should_exit, reason = self.check_exit_conditions(indicators, pos['price'])
+                    #     if should_exit:
+                    #         # 执行卖出
+                    #         order = self.exchange.create_market_sell_order(
+                    #             self.symbol,
+                    #             pos['size']
+                    #         )
+                    #         profit = (indicators['current_price'] - pos['price']) * pos['size']
+                    #         self.trading_positions.remove(pos)
+                    #         logging.info(f"平仓: 原因{reason}, 盈亏{profit}")
                     
                     # 风险检查
                     self.check_risk()
